78dc669341a644bebd9fbaa89d5365800c73760f
MessaoudiHoussam <Messaoudih745@gmail.com>
2021-12-23 21:55:44 +0100

    edite imports

--- chat-me/src/App.jsx
+++ chat-me/src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {streamChat} from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie/es6';
 
-function App() {
+const apiKey = '9p85jrprm2fb';
+const  App = () => {
   return (
-    <div>
-      yo
+    <div className='app_wrapper'>
+      <chat>
+
+      </chat>
     </div>
-  )
+  );
 }
 
 export default App;
